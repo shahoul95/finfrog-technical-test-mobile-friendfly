69cdd8e9cfbba23966d461cf70e994007d46aef4
shahoul95 <shahoul@libheros.fr>
2024-02-04 22:13:24 +0100

    refactor: email regex only fr uk uk

--- src/utils/formRegex.tsx
+++ src/utils/formRegex.tsx
@@ -1,5 +1,5 @@
 const regexFirstNameAndLastName = new RegExp(/^[a-zA-Z\- ]+$/);
-const emailRegex = new RegExp(/^[^\s@]+@[^\s@]+\.[^\s@]+$/);
+const emailRegex = new RegExp(/^[^\s@]+@[^\s@]+\.[[comfrukg]+$/);
 const regexPhoneNumber= new RegExp(/^(?:(?:\+|00)33|0)[67]\d{8}$/);
 
 export {regexFirstNameAndLastName, emailRegex, regexPhoneNumber}
